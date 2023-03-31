168e82038a5ac0b566820b45bf07110eb6e08108
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-31 17:27:10 -0300

    Atualizar

--- js/index.js
+++ js/index.js
@@ -6,19 +6,30 @@ let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 //Menu-Hamburguer
-let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
 
 
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
+    //Vai Remover o Menu Hamburguer
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active')
+
 }
 //Vai Remover menu de busca quanto mover para baixo
-
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+})
+
+//Menu Hamburguer
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 })
 
 //Vai Ativar menu de Busca
