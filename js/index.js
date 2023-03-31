427327fd051c62f8a9b8ffa7ad34c82067e84288
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-31 16:32:48 -0300

    atualizar

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
+//Menu-Hamburguer
+let formClose = document.querySelector('#form-close');
+
+
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
